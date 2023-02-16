b53c951fe8ed3cd3a6950ac426181fe3c61b17b4
SefaYuzuak <112412065+Sefa-Yuzuak@users.noreply.github.com>
2023-02-16 08:33:53 +0300

    Add files via upload

---
+++ src/components/Greeting.js
@@ -0,0 +1,9 @@
+const Greeting = () => {
+    return ( 
+        <div>
+            <h1>hello world</h1>
+            <p>It's good to see you!</p>
+        </div>
+    )
+}
+export default Greeting;
